3ebc19a47764217719f3a178c7d608fd1f4cc25c
Dan Kuehling <dkuehling@district158.org>
2021-09-09 23:58:51 -0500

    Update Bell Schedule.rb
    Rolling in some local changes I hadn't been committing.

--- Bell Schedule.rb
+++ Bell Schedule.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-%w(time pathname rubygems yaml).each{|r| require r}
+%w(time rubygems yaml).each{|r| require r}
 date = Date.today()
 class Integer
 	def ordinal
 		cardinal = self.abs
 		digit = cardinal%10
-		if (1..3).include?(digit) and not (11..13).include?(cardinal%100)
+		if (1..3).include?(digit) and not (11..13).include?(cardinal%100) 
 			self.to_s << %w{st nd rd}[digit-1]
 		else
 			self.to_s << 'th'
@@ -13,25 +13,35 @@ class Integer
 	end
 end
 
-schedule_file = File.open(Pathname(__dir__)+'Schedule.yaml') {|yf| YAML::load(yf)}
+schedule_file = File.open(Pathname(__dir__)+'HS_Schedule.yaml') {|yf| YAML::load(yf)}
 late_starts = schedule_file['late_start_dates']
 this_period = String.new()
 if late_starts.include?(date) then
-  schedule = schedule_file['late_start']
+  schedule_type="late_start"
 else
-  schedule = schedule_file['normal']
+  schedule_type="normal"
 end
+schedule = schedule_file[schedule_type]
 
 schedule.each{|hr|
   start_time = Time.parse(hr['start'])
-  end_time = Time.parse(hr['end'])
+  end_time = Time.parse(hr['end']) 
   time = Time.now()
   if (start_time..end_time).cover?(time)
-     this_period = hr['period'].ordinal+" Period"
+     this_period = hr['period'].ordinal
   end
 }
 unless this_period.nil?
   puts this_period
+end
+
+case schedule_type
+when "late_start"
+  puts "Late Start"
+when "normal"
+  puts "Normal Schedule"
+else
+  puts "Unrecognized Schedule"
 end
 
 schedule.each{|hr|
